7ba62b98f4768d6a79bdfdb412fa4d3c2a49dc7f
blakSou1 <142041470+blakSou1@users.noreply.github.com>
2025-06-26 13:44:57 +0500

    gegege

---
+++ TelegramBot.cs
@@ -0,0 +1,41 @@
+﻿using Telegram.Bot;
+using Telegram.Bot.Polling;
+using Telegram.Bot.Types;
+
+public class TelegramBot
+{
+  private readonly TelegramBotClient _botClient;
+  private readonly string _token = "TOKEN";
+
+  public TelegramBot()
+  {
+      _botClient = new TelegramBotClient(_token);
+  }
+
+  public async Task StartAsync()
+  {
+      var receiverOptions = new ReceiverOptions
+      {
+          AllowedUpdates = { } // Получаем все типы обновлений
+      };
+
+      _botClient.StartReceiving(
+          HandleUpdateAsync,
+          HandleErrorAsync,
+          receiverOptions
+      );
+
+      var me = await _botClient.GetMe();
+      Console.WriteLine($"Бот {me.Username} запущен!");
+  }
+
+    private async Task HandleErrorAsync(ITelegramBotClient client, Exception exception, HandleErrorSource source, CancellationToken token)
+    {
+        throw new NotImplementedException();
+    }
+
+    private async Task HandleUpdateAsync(ITelegramBotClient client, Update update, CancellationToken token)
+    {
+        throw new NotImplementedException();
+    }
+}

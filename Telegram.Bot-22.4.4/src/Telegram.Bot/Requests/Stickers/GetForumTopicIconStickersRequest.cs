0f0498044621b9d1b65ee056cd607a91aecdf637
blakSou1 <142041470+blakSou1@users.noreply.github.com>
2025-06-26 13:36:37 +0500

    нашел че надо нам

--- Telegram.Bot-22.4.4/src/Telegram.Bot/Requests/Stickers/GetForumTopicIconStickersRequest.cs
+++
@@ -1,7 +0,0 @@
-// GENERATED FILE - DO NOT MODIFY MANUALLY
-namespace Telegram.Bot.Requests;
-
-/// <summary>Use this method to get custom emoji stickers, which can be used as a forum topic icon by any user.<para>Returns: An Array of <see cref="Sticker"/> objects.</para></summary>
-[EditorBrowsable(EditorBrowsableState.Never)]
-public partial class GetForumTopicIconStickersRequest() : ParameterlessRequest<Sticker[]>("getForumTopicIconStickers")
-{}
--- Telegram.Bot-22.4.4/src/Telegram.Bot/Types/ForumTopicClosed.cs
+++
@@ -1,5 +0,0 @@
-// GENERATED FILE - DO NOT MODIFY MANUALLY
-namespace Telegram.Bot.Types;
-
-/// <summary>This object represents a service message about a forum topic closed in the chat. Currently holds no information.</summary>
-public partial class ForumTopicClosed;

f8de444c164b97a5027f7be27dd154e03d169612
Shad1qq <124492203+Shad1qq@users.noreply.github.com>
2025-07-15 18:31:58 +0300

    FIx

--- obj/Debug/net9.0/TGBot.AssemblyInfo.cs
+++ obj/Debug/net9.0/TGBot.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("TGBot")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+280c8ba2f18beb19fa8c76ef4023977adf7cceb2")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+469037c9de990562d0cb4e50903bf2bd1ceb2e00")]
 [assembly: System.Reflection.AssemblyProductAttribute("TGBot")]
 [assembly: System.Reflection.AssemblyTitleAttribute("TGBot")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
